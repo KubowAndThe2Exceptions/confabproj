ff20d755e807c2ac484e1394b2be03f9b5b367d6
Ryan Kubow <ryankubow@gmail.com>
2021-12-20 12:27:07 -0800

    Changes to UI design

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,15 +1,24 @@
 class Search < ApplicationRecord
     def search_user
-        user = User.all
-        #Post.find_by name: 'Spartacus', rating: 4 
+        userResults = User.where("lower(name) LIKE ? 
+        OR lower(email) LIKE ? 
+        OR lower(address) LIKE ?", 
+        "%#{name.downcase}%", "%#{email.downcase}%", 
+        "%#{address.downcase}%")
 
-        user = User.where(['name LIKE ?', "%#{self.name}%"]) unless self.name.empty?
-        
-        #where(['name LIKE ?', "%#{self.name}%"]) unless self.name.empty?
-        user = user.where(['email LIKE ?', "%#{self.email}%"]) unless self.email.empty?
-        user = user.where(['address LIKE ?', "%#{self.address}%"]) unless self.address.empty?
-        #user = user.where(['userType LIKE ?', self.userType]) unless self.userType.empty? 
-        #<BROKEN FOR SOME REASON ^^^^>
-        return user
+        return userResults
     end 
 end
+
+# form sends list of input info
+# acquire search fields in list, empty values removed
+
+#dynamic list = take in/out fields based on empty or non-empty input, build SQL dynamically
+#single search + modifier list = search against all fields all the time, but risk pulling
+#back information that isnt useful, geo location vs non-geo location would require a single if statement.
+
+
+
+#1. rollback usertype and make sure its all downcase
+#2. radio button usertype on form page and figure out how to hook up radio
+#3. create query builder, pass through an array of info, concat together into query.
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_11_29_234838) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "searches", force: :cascade do |t|
     t.string "name"
     t.string "email"
