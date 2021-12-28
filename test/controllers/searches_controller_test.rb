5081865d409090220b4d8cd3575872dd69591717
Ryan Kubow <ryankubow@gmail.com>
2021-12-28 14:36:49 -0800

    Made tests for search/controller.rb

--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class SearchesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @search = searches(:one)
+  end
+
+  test "should get new" do
+    get new_search_url
+    assert_response :success
+  end
+
+  test "should create search" do
+    assert_difference('Search.count') do
+      post searches_url, params: { search: { search_input: @search.search_input, usertype: @search.usertype } }
+    end
+    assert_redirected_to search_url(Search.last)
+  end
+
+  test "should show search" do
+    get search_url(@search)
+    assert_response :success
+  end
 end
--- test/models/search_test.rb
+++ test/models/search_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class SearchTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @search = searches(:one)
+    @user = users(:one)
+  end
+  test "search_user returns users" do
+    results = @search.search_user
+    assert_equal(@user.id, results.first.id)
+  end
 end
